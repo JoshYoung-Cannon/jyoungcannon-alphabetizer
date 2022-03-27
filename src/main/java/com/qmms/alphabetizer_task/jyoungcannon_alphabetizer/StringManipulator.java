0a49ed70b5156179cdf3399f307fea1a5f5f85fc
JoshYoung-Cannon <jyoungcannon@gmail.com>
2022-03-27 12:06:42 +0100

    splitString() created and tested

--- src/main/java/com/qmms/alphabetizer_task/jyoungcannon_alphabetizer/StringManipulator.java
+++ src/main/java/com/qmms/alphabetizer_task/jyoungcannon_alphabetizer/StringManipulator.java
@@ -1,10 +1,49 @@
 package com.qmms.alphabetizer_task.jyoungcannon_alphabetizer;
 
-import java.util.ArrayList;
-
 public class StringManipulator {
 
-	public static ArrayList<String> splitString(String sentence, char splitChar){
-		return null;
+	private static int containsCount(String sentence, char splitChar) {
+		int splitCharCount = 0;
+		for(int i = 0; i < sentence.length(); i++) {
+			if ((int) sentence.charAt(i) == (int) splitChar) {
+				splitCharCount += 1;
+			}
+		}
+		return splitCharCount;
+	}
+	
+	private static String copyStringSegment (String sentence, int lowerBound, int upperBound) {
+		String output = "";
+		for (int i = lowerBound; i < upperBound; i++) {
+			output = output + sentence.charAt(i);
+		}
+		return output;
+	}
+	
+	public static String[] splitString(String sentence, char splitChar){
+		int returnCount, lowerBound = 0, upperBound = 0;
+		String[] output;
+		
+		returnCount = containsCount(sentence, splitChar) + 1;
+		
+		output = new String[returnCount];
+		
+		if (returnCount == 1) {
+			output[0] = sentence;
+		}
+		else {
+			for (int i = 0; i < returnCount; i++) {
+				for (int j = lowerBound; j < sentence.length(); j++) {
+					if (sentence.charAt(j) == splitChar) {
+						// j is split point
+						upperBound = j;
+						output[i] = copyStringSegment(sentence, lowerBound, upperBound);
+						lowerBound = j + 1;
+					}
+				}
+			}
+			output[returnCount - 1] = copyStringSegment(sentence, lowerBound, sentence.length());
+		}
+		return output;
 	}
 }
--- src/test/java/com/qmms/alphabetizer_task/jyoungcannon_alphabetizer/StringManipulatorTest.java
+++ src/test/java/com/qmms/alphabetizer_task/jyoungcannon_alphabetizer/StringManipulatorTest.java
@@ -1,19 +1,25 @@
 package com.qmms.alphabetizer_task.jyoungcannon_alphabetizer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 public class StringManipulatorTest {
 
 	@Test
     public void splitStringTest() {
-		ArrayList<String> goal = new ArrayList<>(List.of("Fizz","Buzz"));
-		ArrayList<String> res = StringManipulator.splitString("Fizz-Buzz", '-');
-        assertEquals(goal, res);
+		String[] goal = {"Fizz","Buzz"};
+		String[] res = StringManipulator.splitString("Fizz-Buzz", '-');
+        assertEquals(goal[0], res[0]);
+        assertEquals(goal[1], res[1]);
+        
+        goal = new String[] {"Fizz"};
+        res = StringManipulator.splitString("Fizz", '-');
+        assertEquals(goal[0], res[0]);
     }
+	
+	@Test
+	public void BubbleSortInstanceTest() {
+		StringManipulator SMTest = new StringManipulator();
+		assertEquals(SMTest.getClass(), StringManipulator.class);
+	}
 }
